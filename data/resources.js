2f82953152cd0c2e191c21b0302db597f8726f12
Ger Hobbelt <ger@hobbelt.com>
2019-01-08 01:42:16 +0100

    updated npm packages; added URIs for Unicode 12.0

--- data/resources.js
+++ data/resources.js
@@ -280,6 +280,46 @@ var resources = [
 		'emoji-sequences': 'https://unicode.org/Public/emoji/11.0/emoji-sequences.txt',
 		// Emoji_ZWJ_Sequence
 		'emoji-zwj-sequences': 'https://unicode.org/Public/emoji/11.0/emoji-zwj-sequences.txt',
+		// TR39 Security: homogrpah attack prevention
+		//   http://unicode.org/reports/tr39/restrictions.html
+		//   http://unicode.org/reports/tr39/#Confusable_Detection
+		//   https://dev.to/loganmeetsworld/homographs-attack--5a1p
+		'security-identifier-status': 'https://www.unicode.org/Public/security/11.0.0/IdentifierStatus.txt',
+		'security-identifier-type': 'https://www.unicode.org/Public/security/11.0.0/IdentifierType.txt',
+		'security-confusables': 'https://www.unicode.org/Public/security/11.0.0/confusables.txt',
+		'security-confusables-summary': 'https://www.unicode.org/Public/security/11.0.0/confusablesSummary.txt',
+		'security-intentional': 'https://www.unicode.org/Public/security/11.0.0/intentional.txt',
+	},
+	{
+		'version': '12.0.0',
+		'main': 'https://unicode.org/Public/12.0.0/ucd/UnicodeData-12.0.0d6.txt',
+		'scripts': 'https://unicode.org/Public/12.0.0/ucd/Scripts-12.0.0d15.txt',
+		'script-extensions': 'https://unicode.org/Public/12.0.0/ucd/ScriptExtensions-12.0.0d7.txt',
+		'blocks': 'https://unicode.org/Public/12.0.0/ucd/Blocks-12.0.0d1.txt',
+		'properties': 'https://unicode.org/Public/12.0.0/ucd/PropList-12.0.0d17.txt',
+		'derived-core-properties': 'https://unicode.org/Public/12.0.0/ucd/DerivedCoreProperties-12.0.0d17.txt',
+		'derived-normalization-properties': 'https://unicode.org/Public/12.0.0/ucd/DerivedNormalizationProps-12.0.0d11.txt',
+		'composition-exclusions': 'https://unicode.org/Public/12.0.0/ucd/CompositionExclusions-12.0.0d1.txt',
+		'case-folding': 'https://unicode.org/Public/12.0.0/ucd/CaseFolding-12.0.0d1.txt',
+		'bidi-mirroring': 'https://unicode.org/Public/12.0.0/ucd/BidiMirroring-12.0.0d1.txt',
+		'bidi-brackets': 'https://unicode.org/Public/12.0.0/ucd/BidiBrackets-12.0.0d1.txt',
+		'line-break': 'https://unicode.org/Public/12.0.0/ucd/LineBreak-12.0.0d6.txt',
+		'word-break': 'https://unicode.org/Public/12.0.0/ucd/auxiliary/WordBreakProperty-12.0.0d15.txt',
+		// Emoji, Emoji_Presentation, Emoji_Modifier, Emoji_Modifier_Base, Emoji_Component, Extended_Pictographic
+		'emoji': 'https://unicode.org/Public/emoji/12.0/emoji-data.txt',
+		// Emoji_Keycap_Sequence, Emoji_Flag_Sequence, Emoji_Modifier_Sequence
+		'emoji-sequences': 'https://unicode.org/Public/emoji/12.0/emoji-sequences.txt',
+		// Emoji_ZWJ_Sequence
+		'emoji-zwj-sequences': 'https://unicode.org/Public/emoji/12.0/emoji-zwj-sequences.txt',
+		// TR39 Security: homogrpah attack prevention
+		//   http://unicode.org/reports/tr39/restrictions.html
+		//   http://unicode.org/reports/tr39/#Confusable_Detection
+		//   https://dev.to/loganmeetsworld/homographs-attack--5a1p
+		'security-identifier-status': 'https://www.unicode.org/Public/security/12.0.0/IdentifierStatus.txt',
+		'security-identifier-type': 'https://www.unicode.org/Public/security/12.0.0/IdentifierType.txt',
+		'security-confusables': 'https://www.unicode.org/Public/security/12.0.0/confusables.txt',
+		'security-confusables-summary': 'https://www.unicode.org/Public/security/12.0.0/confusablesSummary.txt',
+		'security-intentional': 'https://www.unicode.org/Public/security/12.0.0/intentional.txt',
 	},
 ];
 
